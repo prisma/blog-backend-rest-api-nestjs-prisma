dfb371d3a35a39bdd0dcbe89b5351ee275abcc47
Tasin Ishmam <tasinishmam@gmail.com>
2022-05-28 17:00:20 +0600

    Finish migration

---
+++ prisma/migrations/20220528101323_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "body" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Article_title_key" ON "Article"("title");
