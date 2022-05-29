d321c639c934778149190227ff79d62938f3829e
Tasin Ishmam <tasinishmam@gmail.com>
2022-05-29 15:19:09 +0600

    Finish add prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,11 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
